fceda1f960012effad30f0d5c76afbfe6cc1c85a
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-05 00:51:18 -0300

    chore: update shadcn theme

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Button } from "../_components/ui/button"
+
+const About = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <Button onClick={() => alert("click")}>Click</Button>
+    </>
+  )
+}
+
+export default About
