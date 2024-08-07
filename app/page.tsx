7b56872f2085eeea6290bbe5f83f6cc399a437b9
Valter B Freitas Jr <valter191@hotmail.com>
2024-08-07 00:52:19 -0300

    feat: add BookingItem component

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@ import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
-import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "./_components/barbershop-item"
+import { quicksearchOptions } from "./_constants/searchOptions"
+import BookingItem from "./_components/booking-item"
 
 export default async function Home() {
   const barbershops = await db.barbershop.findMany({})
@@ -34,50 +33,17 @@ export default async function Home() {
 
         {/* Busca rápida */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image src="/cabelo.svg" width={16} height={16} alt="Cabelo" />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/barba.svg" width={16} height={16} alt="Barba" />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/acabamento.svg"
-              width={16}
-              height={16}
-              alt="Acabamento"
-            />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image src="/massagem.svg" width={16} height={16} alt="Massagem" />
-            Massagem
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/hidratacao.svg"
-              width={16}
-              height={16}
-              alt="Hidração"
-            />
-            Hidratação
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              src="/sobrancelha.svg"
-              width={16}
-              height={16}
-              alt="Sobrancelha"
-            />
-            Sobrancelha
-          </Button>
+          {quicksearchOptions.map((opt) => (
+            <Button className="gap-2" variant="secondary" key={opt.title}>
+              <Image
+                src={opt.imageUrl}
+                width={16}
+                height={16}
+                alt={opt.title}
+              />
+              {opt.title}
+            </Button>
+          ))}
         </div>
 
         {/* Image banner */}
@@ -91,25 +57,7 @@ export default async function Home() {
         </div>
 
         {/* Agendamentos */}
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Toreba Barbershop</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center border-l-2 px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">05</p>
-              <p className="text-sm">23:40</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         {/* Recomendados */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
